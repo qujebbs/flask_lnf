e64b49d5b301f950f4df6be29d546452e91e9993
Asz <126867549+DadaLober@users.noreply.github.com>
2023-11-25 21:42:55 +0800

    add temporary card

--- utils.py
+++ utils.py
@@ -1,29 +1,41 @@
 import mysql.connector
 from flask import session
 
+
 def get_connection():
-    connection = mysql.connector.connect(host="localhost", port="3306", database="lostandfound", user="root", password="105671080088"
-)
+    connection = mysql.connector.connect(
+        host="localhost",
+        port="3306",
+        database="lostandfound",
+        user="root",
+        password="",
+    )
     return connection
+
 
 def close_connection(connection):
     connection.close()
+
 
 def get_cursor():
     connection = get_connection()
     cursor = connection.cursor()
     return cursor, connection
 
+
 def is_user_logged_in():
-    return 'user_id' in session
+    return "user_id" in session
+
 
 def log_in_user(user, user_id, user_role):
-    session['user'] = user
-    session['user_id'] = user_id
-    session['user_role'] = user_role
+    session["user"] = user
+    session["user_id"] = user_id
+    session["user_role"] = user_role
+
 
 def log_out_user():
     session.pop("user", None)
+
 
 def get_current_user_data():
     user = session.get("user")
