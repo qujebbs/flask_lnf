7a9ecde62eced18924beb3b3d73ec47d0b284285
qujebbs <markj00767@gmail.com>
2023-11-26 20:27:08 +0800

    added search

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -17,3 +17,20 @@ def dashboar():
         itemcount = [lostcount, foundcount, unclaimedcount, claimedcount]
         user, user_id, user_role = get_current_user_data()
         return render_template("dashboard.html", user_role= user_role, itemcount=itemcount)
+
+@dashboard.route("/search", methods=["GET", "POST"])
+def search():
+    if not is_user_logged_in():
+        return redirect(url_for("authentication.login"))
+    
+    searchname = request.form["searchbar"]
+    lostcount, foundcount, unclaimedcount, claimedcount = getItemcount()
+    itemcount = [lostcount, foundcount, unclaimedcount, claimedcount]
+    user, user_id, user_role = get_current_user_data()
+    cursor, connection = get_cursor()
+    query = "SELECT u.colUsername, lp.colItemName, lp.colItemDesc, u.colEmail, lp.colDatePosted, pic.colPicURI, lp.colItemID FROM tbl_items AS lp JOIN tbl_user AS u ON lp.colPosterID = u.colUserID JOIN tbl_item_pic AS pic ON lp.colItemID = pic.colItemID and lp.colItemName = %s;"
+    cursor.execute(query, (searchname,))
+    value = cursor.fetchall()
+    img_paths = [row[5] for row in value]
+    img_path = [path.replace('\\', '/') for path in img_paths]
+    return render_template("dashboard.html",user_role=user_role, items=value, paths=img_path, itemcount=itemcount)
