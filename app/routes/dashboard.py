ac3e6e5739abdf791b984d71708695d1b0ea2d80
qujebbs <markj00767@gmail.com>
2023-11-26 20:52:40 +0800

    fixed search

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -24,6 +24,11 @@ def search():
         return redirect(url_for("authentication.login"))
     
     searchname = request.form["searchbar"]
+    if not searchname == '':
+        searched = True
+    else:
+         searched = False
+
     lostcount, foundcount, unclaimedcount, claimedcount = getItemcount()
     itemcount = [lostcount, foundcount, unclaimedcount, claimedcount]
     user, user_id, user_role = get_current_user_data()
@@ -33,4 +38,4 @@ def search():
     value = cursor.fetchall()
     img_paths = [row[5] for row in value]
     img_path = [path.replace('\\', '/') for path in img_paths]
-    return render_template("dashboard.html",user_role=user_role, items=value, paths=img_path, itemcount=itemcount)
+    return render_template("dashboard.html",user_role=user_role, items=value, paths=img_path, itemcount=itemcount, searched=searched)
