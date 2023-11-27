59f9714686e10a72be363521e533a44178dd8e7c
Asz <126867549+DadaLober@users.noreply.github.com>
2023-11-27 14:13:39 +0800

    add alert on update buttons

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,10 +1,12 @@
-document.getElementById('openForm').addEventListener('click', function() {
-  document.getElementById('popUpload').classList.remove('hidden');
+document.getElementById("openForm").addEventListener("click", function () {
+  document.getElementById("popUpload").classList.remove("hidden");
 });
 
-document.getElementById('closeFormButton').addEventListener('click', function() {
-  document.getElementById('popUpload').classList.add('hidden');
-});
+document
+  .getElementById("closeFormButton")
+  .addEventListener("click", function () {
+    document.getElementById("popUpload").classList.add("hidden");
+  });
 
 // document.getElementById('open').addEventListener('click', function() {
 //     document.getElementById('modal').classList.remove('hidden');
@@ -14,14 +16,14 @@ document.getElementById('closeFormButton').addEventListener('click', function() 
 //     document.getElementById('modal').classList.add('hidden');
 //   });
 
-document.addEventListener('click', function (event) {
+document.addEventListener("click", function (event) {
   const target = event.target;
-  if (target.classList.contains('open-button')) {
-      const modal = target.closest('tr').querySelector('.modal');
-      modal.classList.remove('hidden');
-  } else if (target.classList.contains('close-button')) {
-      const modal = target.closest('.modal');
-      modal.classList.add('hidden');
+  if (target.classList.contains("open-button")) {
+    const modal = target.closest("tr").querySelector(".modal");
+    modal.classList.remove("hidden");
+  } else if (target.classList.contains("close-button")) {
+    const modal = target.closest(".modal");
+    modal.classList.add("hidden");
   }
 });
 
@@ -30,39 +32,41 @@ document.addEventListener('click', function (event) {
 //     var words = text.split(/\s+/).filter(function(word) {
 //         return word.length > 0;
 //     });
- 
+
 //     document.getElementById('counter').textContent = words.length ;
 // });
 
-const inputFields = document.querySelectorAll('input[data-te-input-showcounter="true"]');
-const textarea = document.querySelectorAll('textarea[data-te-input-showcounter="true"]');
+const inputFields = document.querySelectorAll(
+  'input[data-te-input-showcounter="true"]'
+);
+const textarea = document.querySelectorAll(
+  'textarea[data-te-input-showcounter="true"]'
+);
 
-  
-  inputFields.forEach(inputField => {
-    const charCount = inputField.nextElementSibling.querySelector('div');
+inputFields.forEach((inputField) => {
+  const charCount = inputField.nextElementSibling.querySelector("div");
 
-    inputField.addEventListener('input', () => {
-      charCount.textContent = `${inputField.value.length} / ${inputField.maxLength}`;
-    });
+  inputField.addEventListener("input", () => {
+    charCount.textContent = `${inputField.value.length} / ${inputField.maxLength}`;
   });
+});
 
-  textarea.forEach(textarea => {
-    const charCount = textarea.nextElementSibling.querySelector('div');
+textarea.forEach((textarea) => {
+  const charCount = textarea.nextElementSibling.querySelector("div");
 
-    textarea.addEventListener('input', () => {
-      charCount.textContent = `${textarea.value.length} / ${textarea.maxLength}`;
-    });
+  textarea.addEventListener("input", () => {
+    charCount.textContent = `${textarea.value.length} / ${textarea.maxLength}`;
   });
+});
 
-
-document.getElementById('fileInput').addEventListener('change', function() {
-  var fileList = document.getElementById('fileList');
-  fileList.innerHTML = ''; // Clear the list
+document.getElementById("fileInput").addEventListener("change", function () {
+  var fileList = document.getElementById("fileList");
+  fileList.innerHTML = ""; // Clear the list
   for (var i = 0; i < this.files.length; i++) {
-      var fileItem = document.createElement('div');
-      fileItem.textContent = this.files[i].name;
-      fileItem.className = 'fileItem';
-      fileList.appendChild(fileItem);
+    var fileItem = document.createElement("div");
+    fileItem.textContent = this.files[i].name;
+    fileItem.className = "fileItem";
+    fileList.appendChild(fileItem);
   }
 });
 
@@ -76,28 +80,71 @@ function toggleForm() {
 }
 function showConfirmationDialog() {
   Swal.fire({
-    title: 'Are you sure?',
-    text: 'You are about to delete this post.',
-    icon: 'warning',
+    title: "Are you sure?",
+    text: "You are about to delete this post.",
+    icon: "warning",
     showCancelButton: true,
-    confirmButtonColor: '#d33',
-    cancelButtonColor: '#3085d6',
-    confirmButtonText: 'Yes, delete it!'
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+    confirmButtonText: "Yes, delete it!",
   }).then((result) => {
     if (result.isConfirmed) {
-      document.getElementById('deleteForm').submit();
+      document.getElementById("deleteForm").submit();
     }
   });
 }
-
-document.getElementById("uploadForm").addEventListener("submit", function(event) {
-  event.preventDefault();
+function updateToUnclaimedDialog() {
   Swal.fire({
-    icon: "success",
-    title: "File has been uploaded successfully",
-    showConfirmButton: false,
-    timer: 1500
-  }).then(function() {
-    event.target.submit();
+    title: "Are you sure you want to mark this as Unclaimed?",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonText: "Yes",
+    cancelButtonText: "No",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Proceed with marking as unclaimed
+      document.getElementById("updateForm").submit();
+    }
   });
-});
+}
+function updateToClaimedDialog() {
+  Swal.fire({
+    title: "Are you sure you want to mark this as Claimed?",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonText: "Yes",
+    cancelButtonText: "No",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Proceed with marking as unclaimed
+      document.getElementById("updateForm").submit();
+    }
+  });
+}
+function updateToFoundDialog() {
+  Swal.fire({
+    title: "Are you sure you want to mark this as Found?",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonText: "Yes",
+    cancelButtonText: "No",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Proceed with marking as unclaimed
+      document.getElementById("updateForm").submit();
+    }
+  });
+}
+document
+  .getElementById("uploadForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    Swal.fire({
+      icon: "success",
+      title: "File has been uploaded successfully",
+      showConfirmButton: false,
+      timer: 1500,
+    }).then(function () {
+      event.target.submit();
+    });
+  });
