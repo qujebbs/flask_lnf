1b87090110c7d266769613250db2a67f935e3067
Asz <126867549+DadaLober@users.noreply.github.com>
2023-11-27 10:17:08 +0800

    fix delete button

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def get_connection():
     connection = mysql.connector.connect(
         host="localhost",
         port="3306",
-        database="lostandfounddb",
+        database="lostandfound",
         user="root",
         password="",
     )
