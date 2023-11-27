1f01aaa9e401b5d84941c587b69a8e275885fddf
qujebbs <markj00767@gmail.com>
2023-11-27 11:16:11 +0800

    fixed post display

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -33,9 +33,9 @@ def search():
     itemcount = [lostcount, foundcount, unclaimedcount, claimedcount]
     user, user_id, user_role = get_current_user_data()
     cursor, connection = get_cursor()
-    query = "SELECT u.colUsername, lp.colItemName, lp.colItemDesc, u.colEmail, lp.colDatePosted, pic.colPicURI, lp.colItemID, lp.colStatusID FROM tbl_items AS lp JOIN tbl_user AS u ON lp.colPosterID = u.colUserID JOIN tbl_item_pic AS pic ON lp.colItemID = pic.colItemID and lp.colItemName = %s;"
-    cursor.execute(query, (searchname,))
+    query = "SELECT u.colUsername, lp.colItemName, lp.colItemDesc, u.colEmail, lp.colDatePosted, pic.colPicURI, lp.colItemID, lp.colStatusID FROM tbl_items AS lp JOIN tbl_user AS u ON lp.colPosterID = u.colUserID left JOIN tbl_item_pic AS pic ON lp.colItemID = pic.colItemID and lp.colItemName = %s where lp.colItemName = %s;"
+    cursor.execute(query, (searchname,searchname,))
     value = cursor.fetchall()
-    img_paths = [row[5] for row in value]
+    img_paths = [row[5] if row[5] is not None else "No Image" for row in value]
     img_path = [path.replace('\\', '/') for path in img_paths]
     return render_template("dashboard.html",user_role=user_role, items=value, paths=img_path, itemcount=itemcount, searched=searched)
