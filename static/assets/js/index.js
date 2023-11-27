2086edde4979a7fc8fbafbe31a23d6dba28d3230
Asz <126867549+DadaLober@users.noreply.github.com>
2023-11-27 10:00:37 +0800

    added delete prompt

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -74,3 +74,18 @@ function toggleForm() {
     formContainer.style.display = "none";
   }
 }
+function showConfirmationDialog() {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: 'You are about to delete this post.',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.getElementById('deleteForm').submit();
+    }
+  });
+}
